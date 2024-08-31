d6fa0486bfc729759b7abe12f0312e3aab5b8d9c
Mr20DL <andresmonte175@gmail.com>
2024-08-31 01:23:57 -0500

    Primera confirmación

---
+++ main.py
@@ -0,0 +1,79 @@
+import pygame
+import constantes
+from personaje import Personaje
+
+pygame.init()
+
+ventana = pygame.display.set_mode((constantes.ANCHO_VENTANA,constantes.ALTO_VENTANA))
+
+pygame.display.set_caption("Jueguito")
+
+player_image = pygame.image.load("assets//images//characters//player//perrito.jpg")
+
+jugador = Personaje(50,50)
+
+#definir variables de movimiento del jugador
+mover_arriba = False
+mover_abajo = False
+mover_izquierda = False
+mover_derecha = False
+
+#controlar el frame rate
+reloj = pygame.time.Clock()
+
+run = True
+while run == True:
+
+    #QUE VAYA A 60 FPS
+    reloj.tick(constantes.FPS)
+
+
+    ventana.fill(constantes.COLOR_BG)
+
+    #Calcular movimiento de jugador
+    delta_x = 0
+    delta_y = 0
+
+    if mover_derecha == True:
+        delta_x = constantes.VELOCIDAD
+    if mover_izquierda == True:
+        delta_x = -constantes.VELOCIDAD
+    if mover_arriba == True:
+        delta_y = -constantes.VELOCIDAD
+    if mover_abajo == True:
+        delta_y = constantes.VELOCIDAD
+
+    #mover al jugador
+
+    jugador.movimiento(delta_x, delta_y)
+
+    jugador.draw(ventana)
+
+    for event in pygame.event.get():
+        #Para cerrar el juego
+        if event.type == pygame.QUIT:
+            run = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                mover_izquierda = True
+            if event.key == pygame.K_d:
+                mover_derecha = True
+            if event.key == pygame.K_w:
+                mover_arriba = True
+            if event.key == pygame.K_s:
+                mover_abajo = True
+
+        #Para cuando se suelta la tecla
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                mover_izquierda = False
+            if event.key == pygame.K_d:
+                mover_derecha = False
+            if event.key == pygame.K_w:
+                mover_arriba = False
+            if event.key == pygame.K_s:
+                mover_abajo = False
+
+    pygame.display.update()
+pygame.quit()
---
+++ personaje.py
@@ -0,0 +1,14 @@
+import pygame
+import constantes
+
+class Personaje():
+    def __init__(self, x, y):
+        self.shape = pygame.Rect(0, 0, constantes.ANCHO_PERSONAJE, constantes.ALTO_PERSONAJE)
+        self.shape.center = (x,y)
+
+    def movimiento(self, delta_x, delta_y):
+        self.shape.x = self.shape.x + delta_x
+        self.shape.y = self.shape.y + delta_y
+
+    def draw(self, interfaz):
+        pygame.draw.rect(interfaz, constantes.COLOR_PERSONAJE, self.shape)
